f3269af23aa6ccdfb776da04dde791852f4038bf
Greg <harrison.greg.c@gmail.com>
2017-12-10 23:31:21 -0500

    minor addition

--- react-flashcards/src/store/ducks/user/reducers.js
+++ react-flashcards/src/store/ducks/user/reducers.js
@@ -23,11 +23,23 @@ const initialState = {
 const recordListReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.FETCH_USER: {
+      // This will pull the user from the database
+
       // return { ...state, user: action.payload.data.data.user }
     }
 
     case types.CREATE_USER: {
+      // Use Name, Email and Password to create a user in the backend
+
       // return state.user = state.user.push(action.payload.draft)
+    }
+
+    case types.INPUT_USER_NAME: {
+      // Updating the typing of a user name
+    }
+
+    case types.INPUT_USER_EMAIL: {
+      // Updating the user email during input - run validations and so on
     }
 
     default: return state
