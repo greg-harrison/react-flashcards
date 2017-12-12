b45da81597be28219f3aa7fd963356e9f0e6542a
Greg <harrison.greg.c@gmail.com>
2017-12-11 22:05:57 -0500

    Added child component for Card

---
+++ react-flashcards/src/components/card/cardTItle/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CardTitle = ({ title }) => {
+  return (
+    <div>
+      <h3>{title}</h3>
+    </div>
+  )
+}
+
+export default CardTitle
--- react-flashcards/src/components/card/index.js
+++ react-flashcards/src/components/card/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
+import CardTitle from './cardTitle'
 
 class Card extends Component {
   render() {
     return (
       <div className="card container m-0">
+        <CardTitle title="Test" />
         <h1>Hello world</h1>
       </div>
     )
