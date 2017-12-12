a965552c231e715b1165b738ffbad0e9fdb1d818
Greg <harrison.greg.c@gmail.com>
2017-12-11 22:01:27 -0500

    Added Card component

---
+++ react-flashcards/src/components/card/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Card extends Component {
+  render() {
+    return (
+      <div className="card container m-0">
+        <h1>Hello world</h1>
+      </div>
+    )
+  }
+}
+
+export default Card
