1f428dee31ba5c47c5765ec5b0739a08e7b46f48
Greg <harrison.greg.c@gmail.com>
2017-12-26 19:37:03 -0500

    Added Signup comp

--- react-flashcards/src/App.js
+++ react-flashcards/src/App.js
@@ -3,6 +3,7 @@ import './style/App.css'
 import Header from './components/base_components/header'
 import Landing from './components/landing'
 import Login from './components/user/login'
+import Signup from './components/user/signup'
 import Stack from './components/stack'
 import { BrowserRouter as Router, Link, Render, Route } from 'react-router-dom'
 
@@ -22,6 +23,7 @@ class App extends Component {
           <Route exact={true} path='/' component={Landing} />
           <Route path='/stack/:stackId' component={Stack} />
           <Route exact={true} path='/login' component={Login} />
+          <Route exact={true} path='/signup' component={Signup} />
         </div>
       </Router>
     );
--- react-flashcards/src/components/user/signup/index.js
+++ react-flashcards/src/components/user/signup/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-import './login.css'
+import './signup.css'
 
-export default class Login extends Component {
+export default class Signup extends Component {
   render() {
     return (
-      <div className="login justify-content-center m-0 p-0 text-center">
+      <div className="signup justify-content-center m-0 p-0 text-center">
         <div className="card col-md-6 p-0">
           <div className="card-body">
             <form>
