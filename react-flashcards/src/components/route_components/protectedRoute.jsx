a3426047c0ac72a5b79cf765ce34dfbfe71877e3
Greg <greg@Gregs-MacBook-Pro.local>
2018-01-05 07:26:15 -0500

    Protected route in React Router

---
+++ react-flashcards/src/components/route_components/protectedRoute.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Render, Route, Redirect } from 'react-router-dom'
+
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100)
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
+
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route {...rest} render={(props) => (
+    fakeAuth.isAuthenticated === true
+      ? <Component {...props} />
+      : <Redirect to='/login' />
+  )} />
+)
+
+export default PrivateRoute
