9fb5dc3b379dd545a1fa2202ae451102db04e98d
Greg <greg@Gregs-MacBook-Pro.local>
2018-03-08 07:25:15 -0500

    Updated test suite file. Bypassing faked auth for now

--- react-flashcards/src/helpers/helpers.test.js
+++ react-flashcards/src/helpers/helpers.test.js
@@ -1,21 +1,25 @@
 import { verifyEmail, verifyPassword } from './index'
 
-it('Email returns Invalid for ajkdjsljdsa', () => {
-  let email = 'ajkdjsljdsa'
-  expect(verifyEmail(email)).toBe(false)
-});
+describe('Email', () => {
+  it('Email returns Invalid for ajkdjsljdsa', () => {
+    let email = 'ajkdjsljdsa'
+    expect(verifyEmail(email)).toBe(false)
+  });
 
-it('Email returns Valid for ajkdjsljdsa@test.com', () => {
-  let email = 'ajkdjsljdsa@test.com'
-  expect(verifyEmail(email)).toBe(true)
-});
+  it('Email returns Valid for ajkdjsljdsa@test.com', () => {
+    let email = 'ajkdjsljdsa@test.com'
+    expect(verifyEmail(email)).toBe(true)
+  });
+})
 
-it('Password returns Invalid for Test', () => {
-  let pw = 'ajkdjsljdsa'
-  expect(verifyPassword(pw)).toBe(false)
-});
+describe('Password', () => {
+  it('Password returns Invalid for Test', () => {
+    let pw = 'ajkdjsljdsa'
+    expect(verifyPassword(pw)).toBe(false)
+  });
 
-it('Password returns Valid for ajkdjsljdsa@test.com', () => {
-  let pw = 'asjdklAJK23@32@'
-  expect(verifyPassword(pw)).toBe(true)
-});
+  it('Password returns Valid for ajkdjsljdsa@test.com', () => {
+    let pw = 'asjdklAJK23@32@'
+    expect(verifyPassword(pw)).toBe(true)
+  });
+})
